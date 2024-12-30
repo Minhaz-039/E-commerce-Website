46b47b9ef4508798b2cff15e3b2859ca63a80ad7
Minhaz-039 <minhazreza3243@gmail.com>
2024-12-31 05:25:42 +0600

    log in , logout , register created

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -10,23 +10,46 @@ import { FaHeart } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import './Navigation.css';
+import { useDispatch, useSelector } from "react-redux";
+import { useLogoutMutation } from "../../redux/api/userApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+
+
 
 
 const Navigation = () => {
+
+    const { userInfo } = useSelector(state => state.auth);
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showsidebar, setShowsidebar] = useState(false);
 
     const toggleDropdown = () => {
         setDropdownOpen(!dropdownOpen);
-    }
+    };
 
     const togglesidebar = () => {
         setShowsidebar(!showsidebar);
-    }
+    };
 
     const closeSidebar = () => {
         setShowsidebar(false);
+    };
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [logoutApiCall] = useLogoutMutation();
+
+    const logoutHandler = async () => {
+        try {
+            await logoutApiCall().unwrap();
+            dispatch(logout());
+            navigate('/login');
+        } catch (error) {
+            console.error(error);
+        }
     }
 
     return (
@@ -37,49 +60,109 @@ const Navigation = () => {
 
 
                 <Link to="/" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineHome className="mr-2 mt-[3rem]" size={26}/>
+                    <AiOutlineHome className="mr-2 mt-[3rem]" size={26} />
                     <span className="hidden nav-item-name mt-[3rem]">HOME</span>{" "}
                 </Link>
 
 
                 <Link to="/shop" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineShopping className="mr-2 mt-[3rem]" size={26}/>
+                    <AiOutlineShopping className="mr-2 mt-[3rem]" size={26} />
                     <span className="hidden nav-item-name mt-[3rem]">SHOP</span>{" "}
                 </Link>
 
 
                 <Link to="/cart" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26}/>
+                    <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
                     <span className="hidden nav-item-name mt-[3rem]">CART</span>{" "}
                 </Link>
 
 
                 <Link to="/favorite" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <FaHeart className="mr-2 mt-[3rem]" size={26}/>
+                    <FaHeart className="mr-2 mt-[3rem]" size={26} />
                     <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>{" "}
                 </Link>
 
+            </div>
+
+
+            <div className="relative">
+                <button onClick={toggleDropdown} className="flex items-center text-gray-8000 focus:outline-none">
+                    {userInfo ? (<span className="text-white">{userInfo.username}</span>) : (<></>)}
+                    {userInfo && (dropdownOpen ? <IoMdArrowDropdown /> : <IoMdArrowDropup />)}
+                </button>
+
+                {dropdownOpen && userInfo && (
+                    <ul className={`absolute right-0 mt-2 mr-14 bg-black text-white p-2 ${!userInfo.isAdmin ? "-top-20" : "-top-80"}`}>
+                        {userInfo.isAdmin && (
+                            <>
+                                <li>
+                                    <Link to='/admin/dashboard' className="block px-4 py-2 hover:bg-gray-100">
+                                        Dashboard
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/admin/productlist' className="block px-4 py-2 hover:bg-gray-100">
+                                        Products
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/admin/categorylist' className="block px-4 py-2 hover:bg-gray-100">
+                                        Category
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/admin/orderlist' className="block px-4 py-2 hover:bg-gray-100">
+                                        Orders
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to='/admin/userlist' className="block px-4 py-2 hover:bg-gray-100">
+                                        Users
+                                    </Link>
+                                </li>
+
+                            </>
+                        )}
+
+
+                        <li>
+                            <Link to='/admin/profile' className="block px-4 py-2 hover:bg-gray-100">
+                                Profile
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/admin/logout' onClick={logoutHandler} className="block px-4 py-2 hover:bg-gray-100">
+                                Logout
+                            </Link>
+                        </li>
+
+
+                    </ul>)}
 
             </div>
 
-            <ul>
+            {!userInfo && (
+                <ul>
 
-                <li>
-                <Link to="/login" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineLogin className="mr-2 mt-[3rem]" size={26}/>
-                    <span className="hidden nav-item-name mt-[3rem]">Log In</span>{" "}
-                </Link>
-                </li>
+                    <li>
+                        <Link to="/login" className="flex items-center transition-transform transform hover:translate-x-2">
+                            <AiOutlineLogin className="mr-2 mt-[3rem]" size={26} />
+                            <span className="hidden nav-item-name mt-[3rem]">Log In</span>{" "}
+                        </Link>
+                    </li>
 
 
-                <li>
-                <Link to="/register" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26}/>
-                    <span className="hidden nav-item-name mt-[3rem]">Register</span>{" "}
-                </Link>
-                </li>
+                    <li>
+                        <Link to="/register" className="flex items-center transition-transform transform hover:translate-x-2">
+                            <AiOutlineUserAdd className="mr-2 mt-[3rem]" size={26} />
+                            <span className="hidden nav-item-name mt-[3rem]">Register</span>{" "}
+                        </Link>
+                    </li>
 
-            </ul>
+                </ul>
+            )}
+
+
 
 
         </div>
