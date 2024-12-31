8929d88a3593dbc9acd8c441a4125ff631c6d3f7
Minhaz-039 <minhazreza3243@gmail.com>
2024-12-31 08:11:29 +0600

    Update operation added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,18 +5,30 @@ import {Route , RouterProvider , createRoutesFromElements} from 'react-router'
 import { createBrowserRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import Register from './pages/Auth/Register.jsx'
+import Profile from './pages/User/Profile.jsx'
+
+
+//Private Routes
+import PrivateRoutes from './components/PrivateRoutes.jsx'
 
 
 // Auth
 import LogIn from './pages/Auth/LogIn.jsx'
+import Register from './pages/Auth/Register.jsx'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />} >
+
+      <Route path='' element={<PrivateRoutes />} >
+        <Route path='/profile' element={<Profile />} />
+      </Route>
+
+
       <Route path='/login' element={<LogIn />} />
       <Route path='/register' element={<Register />} />
+
     </Route>
   )
 );
