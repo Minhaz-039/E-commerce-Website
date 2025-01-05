85fa39b4924b180ee55795059fd22acb6a91851a
Minhaz-039 <minhazreza3243@gmail.com>
2025-01-06 00:32:37 +0600

    Shop added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,8 @@ import AllProducts from './pages/Admin/AllProducts.jsx'
 import Home from './pages/Home.jsx'
 import Favorites from './pages/Products/Favorites.jsx'
 import ProductDetails from './pages/Products/ProductDetails.jsx'
+import Cart from './pages/Cart.jsx'
+import Shop from './pages/Shop.jsx'
 
 //Private Routes
 import PrivateRoutes from './components/PrivateRoutes.jsx'
@@ -36,6 +38,8 @@ const router = createBrowserRouter(
       <Route index={true} path='/' element={<Home />} />
       <Route path='favorite' element={<Favorites/>} />
       <Route path='/product/:id' element={<ProductDetails/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/shop' element={<Shop/>}/>
 
 
       <Route path='' element={<PrivateRoutes />} >
--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -22,6 +22,7 @@ import FavoritesCount from "../Products/FavoritesCount";
 const Navigation = () => {
 
     const { userInfo } = useSelector(state => state.auth);
+    const { cartItems } = useSelector((state) => state.cart)
 
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [showsidebar, setShowsidebar] = useState(false);
@@ -72,16 +73,32 @@ const Navigation = () => {
                 </Link>
 
 
-                <Link to="/cart" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <AiOutlineShoppingCart className="mr-2 mt-[3rem]" size={26} />
-                    <span className="hidden nav-item-name mt-[3rem]">CART</span>{" "}
+                <Link to="/cart" className="flex relative">
+                    <div className="flex items-center transition-transform transform hover:translate-x-2">
+                        <AiOutlineShoppingCart className="mt-[3rem] mr-2" size={26} />
+                        <span className="hidden nav-item-name mt-[3rem]">Cart</span>{" "}
+                    </div>
+
+                    <div className="absolute top-9">
+                        {cartItems.length > 0 && (
+                            <span>
+                                <span className="px-1 py-0 text-sm text-white bg-pink-500 rounded-full">
+                                    {cartItems.reduce((a, c) => a + c.qty, 0)}
+                                </span>
+                            </span>
+                        )}
+                    </div>
                 </Link>
 
 
                 <Link to="/favorite" className="flex items-center transition-transform transform hover:translate-x-2">
-                    <FaHeart className="mr-2 mt-[3rem]" size={26} />
-                    <span className="hidden nav-item-name mt-[3rem]">FAVORITE</span>{" "}
-                    <FavoritesCount/>
+                    <div className="flex justify-center items-center transition-transform transform hover:translate-x-2">
+                        <FaHeart className="mt-[3rem] mr-2" size={20} />
+                        <span className="hidden nav-item-name mt-[3rem]">
+                            Favorites
+                        </span>{" "}
+                        <FavoritesCount />
+                    </div>
                 </Link>
 
             </div>
